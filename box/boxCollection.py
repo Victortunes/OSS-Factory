3465450718af0ebad4b34adb12004e5dd4055641
Victortunes <77933559+Victortunes@users.noreply.github.com>
2023-07-03 23:55:29 +0200

    added boxCollection class and tests -> collection management of boxes

---
+++ box/boxCollection.py
@@ -0,0 +1,36 @@
+from box import Box
+
+class BoxCollection:
+    def __init__(self) -> None:
+        self.__collection: set = set()
+
+    def __iter__(self):
+        return self.__collection.__iter__()
+
+    def __data_validation_box(self, box: Box) -> None:
+        if type(box) != Box: raise TypeError('box must be a Box object')
+
+    def register(self, box: Box) -> None:
+        self.__data_validation_box(box)
+        if box in self.__collection: raise BoxAlreadyExistInCollection(box.id)
+        self.__collection.add(box)
+
+    def unregister(self, box: Box) -> None:
+        self.__data_validation_box(box)
+        try: self.__collection.remove(box)
+        except KeyError: raise BoxNotExistInCollection(box.id)
+
+    @property
+    def collection(self) -> set:
+        return self.__collection.copy()
+
+
+class BoxNotExistInCollection(KeyError):
+     def __init__(self, box_id: str):
+        self.message = f'Box id : {box_id}'
+        super().__init__(self.message)
+
+class BoxAlreadyExistInCollection(KeyError):
+     def __init__(self, box_id: str):
+        self.message = f'Box id : {box_id}'
+        super().__init__(self.message)
---
+++ box/test_boxCollection.py
@@ -0,0 +1,67 @@
+import unittest
+import random
+
+from boxCollection import BoxCollection, BoxAlreadyExistInCollection, BoxNotExistInCollection
+from box import Box
+from boxId import BoxId
+
+class TestBoxCollectionEmpty(unittest.TestCase):
+
+    def setUp(self):
+        self.__box_collection = BoxCollection()
+
+    def test_register_box(self):
+        box_set = set()
+        for __ in range(10):
+            box = Box(BoxId.get_random_box_id().id)
+            box_set.add(box)
+            try: self.__box_collection.register(box)
+            except BoxAlreadyExistInCollection: pass 
+
+        self.assertEqual(box_set, self.__box_collection.collection)
+
+    def test_register_box_wrong_datatype(self):
+        with self.assertRaises(TypeError):
+            self.__box_collection.register(1)
+
+
+
+class TestBoxCollectionPrefilled(unittest.TestCase):
+
+    def setUp(self):
+        self.__box_collection = BoxCollection()
+        self.__prefilled_boxes: set[Box] = set()
+        for __ in range(10):
+            box = Box(BoxId.get_random_box_id().id)
+            self.__prefilled_boxes.add(box)
+            try: self.__box_collection.register(box)
+            except BoxAlreadyExistInCollection: pass 
+
+
+    def test_for_loop(self):
+        collected_box = set()
+        for box in self.__box_collection:
+            collected_box.add(box)
+        self.assertEqual(collected_box,self.__prefilled_boxes)
+
+    def test_no_side_effects(self):
+        copied_collection = self.__box_collection.collection
+        copied_collection.pop()
+        unmodified_collection = self.__box_collection.collection
+        self.assertNotEqual(copied_collection, unmodified_collection)
+
+    def test_unregister_box(self):
+        box_to_remove = self.__box_collection.collection.pop()
+        self.__box_collection.unregister(box_to_remove) #ok
+        with self.assertRaises(BoxNotExistInCollection):
+            self.__box_collection.unregister(box_to_remove) #should fail, already deleted
+    
+    def test_unregister_box_wrong_datatype(self):
+        with self.assertRaises(TypeError):
+            self.__box_collection.unregister(1)
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()

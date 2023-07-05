02a78cf9d5a2d6ea9bcbdd6d3ca4a114499f659e
Victortunes <77933559+Victortunes@users.noreply.github.com>
2023-07-05 11:29:49 +0200

    modified to use the controller

--- main.py
+++ main.py
@@ -1,30 +1,11 @@
-from storage.fileStorage import FileStorage
-from utils import common_letters
+from controller import Controller
+from display import Display
 
-def run():
-    path = './storage/input.txt'
+def main():
+    display = Display()
+    controller = Controller(display)
 
-    file_storage = FileStorage(path)
+    controller.run('./storage/input.txt')
 
-    box_collection, unregistered_boxes = file_storage.register_into_box_collection()
-    cheksum = box_collection.checksum
-
-    print('-'*6 + ' Part 1 ' + '-'*6)
-
-    if unregistered_boxes:
-        print('Warning somes boxes hasn\'t be imported')
-        for unregistered_box in unregistered_boxes:
-            print(f'Box id : {unregistered_box["box_id"]} Reason : {unregistered_box["error"]}')
-
-    print(f'Total registered boxes : {len(box_collection)}')
-    print(f'Cheksum : {cheksum}')
-
-    print('-'*6 + ' Part 2 ' + '-'*6)
-    near_boxes_list = box_collection.get_near_box_ids()
-    for near_boxes in near_boxes_list:
-        box_id_a = near_boxes[0].id
-        box_id_b = near_boxes[1].id
-        similiraties = common_letters(box_id_a, box_id_b)
-        print(f'Box id a/b : {box_id_a} / {box_id_b} -> {similiraties}')
-
-run()
+if __name__ == '__main__':
+    main()

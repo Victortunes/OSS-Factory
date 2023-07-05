495e0e156a08888dd7e935c6a7aeaf3d83eb8d74
Victortunes <77933559+Victortunes@users.noreply.github.com>
2023-07-05 11:01:11 +0200

    added display with its tests -> class that used to display content to the user

---
+++ display.py
@@ -0,0 +1,82 @@
+import logging
+
+class Display:
+    """
+    A class that used to display content to the user.
+
+    Methods
+    -------
+    title -> None
+        display a title with the provided message
+    print -> None
+        print the provided message
+    warn -> None
+        display a warn with the provided message
+    error -> None
+        display an error with the provided message
+    question_with_answer -> None
+        display a question with his answer
+    
+    Property
+    --------
+    logger -> logging.Logger
+        return the logger used by the display
+    """
+
+    def __init__(self) -> None:
+        logging.basicConfig(level=logging.INFO)
+
+    def __data_validation_string(self, data:str, data_name:str) -> None:
+        """Validate string data, raise an error if not valid"""
+        if type(data) is not str: raise TypeError(f'{data_name} must be a string')
+
+    def __data_validation_char(self, data:str, data_name:str) -> None:
+        """Validate char data, raise an error if not valid"""
+        if type(data) is not str: raise TypeError(f'{data_name} must be a string')
+        if len(data) != 1:raise ValueError(f'{data_name} must be one length')
+
+    def __data_validation_int(self, data:int, data_name:str, min:int|None = None, max:int|None = None) -> None:
+        """Validate int data with optional range, raise an error if not valid"""
+        if type(data) is not int: raise TypeError(f'{data_name} must be an int')
+        if min is not None: 
+            if data < min: raise ValueError(f'{data_name} must be greater than {min}')
+        if max is not None: 
+            if data > max: raise ValueError(f'{data_name} must be less than {max}')
+
+    def title(self, title:str, stroke:str = '-', stroke_len:int = 6):
+        """display a title with the provided message"""
+        self.__data_validation_string(title, 'title')
+        self.__data_validation_char(stroke, 'stroke')
+        self.__data_validation_int(stroke_len, 'stroke_len', min=0)
+        title = f' {title} '
+        title = stroke*stroke_len + title + stroke*stroke_len
+        self.print(title)
+
+    def print(self, message:str):
+        """print the provided message"""
+        self.__data_validation_string(message, 'message')
+        print(message)
+
+    def warn(self, message:str):
+        """display a warn with the provided message"""
+        self.__data_validation_string(message, 'message')
+        logging.warn(f' {message}')
+
+    def error(self, message:str):
+        """display an error with the provided message"""
+        self.__data_validation_string(message, 'message')
+        logging.error(f' {message}')
+
+    def question_with_answer(self, question:str, answer:str, answer_arrow:str='=>'):
+        """display a question with his answer"""
+        self.__data_validation_string(question, 'question')
+        self.__data_validation_string(answer, 'answer')
+        self.__data_validation_string(answer_arrow, 'answer_arrow')
+
+        self.print(question)
+        self.print(f'{answer_arrow} {answer}')
+
+    @property
+    def logger(self) -> logging.Logger:
+        """return the logger used by the display"""
+        return logging.getLogger(__name__)
---
+++ test_display.py
@@ -0,0 +1,121 @@
+import unittest
+from io import StringIO
+import sys
+import logging
+
+from display import Display
+
+class TesDisplay(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.__display = Display()
+        self.__capturedOutput = StringIO() #redirect and capture print() output
+        sys.stdout = self.__capturedOutput    
+
+    def tearDown(self) -> None:
+        sys.stdout = sys.__stdout__ #reset redirect
+
+    def test_valid_logger_proprerty(self):
+        self.assertEqual(type(self.__display.logger), logging.Logger)
+
+    def test_valid_info(self):
+        expected = 'abc'
+        self.__display.print(expected)
+        self.assertIn(expected, self.__capturedOutput.getvalue())
+
+    def test_wrong_datatype_info(self):
+        with self.assertRaises(TypeError):
+            self.__display.print(1) #type: ignore
+
+    def test_valid_warn(self):
+        expected = 'abc'
+        with self.assertLogs(level=logging.WARN) as log:
+            self.__display.warn(expected)
+            self.assertEqual(len(log.output), 1)
+            self.assertEqual(len(log.records), 1)
+            self.assertIn(expected, log.output[0])
+
+    def test_wrong_datatype_warn(self):
+        with self.assertRaises(TypeError):
+            self.__display.warn(1) #type: ignore
+
+    def test_valid_error(self):
+        expected = 'abc'
+        with self.assertLogs(level=logging.ERROR) as log:
+            self.__display.error(expected)
+            self.assertEqual(len(log.output), 1)
+            self.assertEqual(len(log.records), 1)
+            self.assertIn(expected, log.output[0])
+
+    def test_wrong_datatype_error(self):
+        with self.assertRaises(TypeError):
+            self.__display.error(1) #type: ignore
+
+    def test_valid_title(self):
+        self.__display.title('abc', '=', 2)
+        self.assertEqual(self.__capturedOutput.getvalue(), '== abc ==\n')
+
+    def test_wrong_message_datatype_title(self):
+        with self.assertRaises(TypeError):
+            self.__display.title(1) #type: ignore
+
+    def test_wrong_stroke_datatype_title(self):
+        with self.assertRaises(TypeError):
+            self.__display.title('abc', 1) #type: ignore
+
+    def test_too_long_stroke_title(self):
+        with self.assertRaises(ValueError):
+            not_a_char = 'a_'
+            self.__display.title('abc', not_a_char)
+
+    def test_wrong_stroke_len_datatype_title(self):
+        with self.assertRaises(TypeError):
+            self.__display.title('abc', stroke_len='1') #type: ignore
+    
+    def test_out_of_range_stroke_len_title(self):
+        with self.assertRaises(ValueError):
+            self.__display.title('abc', stroke_len=-1)
+
+    def test_all_invalid_title(self):
+        with self.assertRaises( (ValueError, TypeError) ):
+            self.__display.title(1,1,'1') #type: ignore
+
+    def test_valid_question_with_answer(self):
+        question = '1+1 ?'
+        answer = '2'
+        arrow = '::>'
+        self.__display.question_with_answer(question, answer, arrow)
+        actual = self.__capturedOutput.getvalue()
+
+        #content check
+        self.assertIn(question, actual)
+        self.assertIn(answer, actual)
+        self.assertIn(arrow, actual)
+
+        #format check
+        splitted_actual = actual.split('\n')
+        actual_question = splitted_actual[0]
+        actual_answer_with_arrow = splitted_actual[1]
+
+        self.assertEqual(question, actual_question)
+        self.assertTrue(actual_answer_with_arrow.startswith(arrow))
+        self.assertTrue(actual_answer_with_arrow.endswith(answer))
+
+    def test_wrong_question_datatype_question_with_answer(self):
+        with self.assertRaises(TypeError):
+            self.__display.question_with_answer(1+1, '2') #type: ignore
+
+    def test_wrong_answer_datatype_answer_with_answer(self):
+        with self.assertRaises(TypeError):
+            self.__display.question_with_answer('1+1', 2) #type: ignore
+
+    def test_wrong_arrow_datatype_questin_with_answer(self):
+        with self.assertRaises(TypeError):
+            self.__display.question_with_answer('1+1', '2', 1) #type: ignore
+
+    def test_all_invalid_question_with_answer(self):
+        with self.assertRaises(TypeError):
+            self.__display.question_with_answer(1+1, 2, 1) #type: ignore
+
+if __name__ == '__main__':
+    unittest.main()
